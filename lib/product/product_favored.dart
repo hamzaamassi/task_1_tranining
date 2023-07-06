89c9a9b2fa0a671cbd2822511fadff4db33abe4e
Hamza Amassi <95129785+hamzaamassi@users.noreply.github.com>
2023-07-06 14:35:54 +0300

    change some class name

--- lib/product/product_favored.dart
+++ lib/product/product_favored.dart
@@ -2,8 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:get/get.dart';
 
-class ProductDetails extends StatelessWidget {
-  const ProductDetails({super.key});
+class ProductFavored extends StatelessWidget {
+  const ProductFavored({super.key});
 
   @override
   Widget build(BuildContext context) {
